5400f589631b875907c4ecd42bab3fe686657a0f
Xue Ming Chen <xueming.chen@tu-dortmund.de>
2023-12-14 09:26:12 +0100

    added RootService

---
+++ src/main/kotlin/service/RootService.kt
@@ -0,0 +1,18 @@
+package service
+
+import entity.Indigo
+
+/**
+ *  The class [RootService] is the service class which connect the service layer
+ *  with the entity layer
+ */
+class RootService {
+    /**
+     * @property currentGame The property currentGame have saved the current Indigo Game
+     */
+    var currentGame: Indigo? = null
+    /**
+     *  @property networkService The NetworkService
+     */
+    val networkService = NetworkService(this)
+}
