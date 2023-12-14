8a69fa12abcd109ef8ae808957da8b1054d6f57f
Xue Ming Chen <xueming.chen@tu-dortmund.de>
2023-12-14 09:35:34 +0100

    added documentation

--- src/main/kotlin/service/IndigoNetworkClient.kt
+++ src/main/kotlin/service/IndigoNetworkClient.kt
@@ -3,6 +3,15 @@ package service
 import tools.aqua.bgw.net.client.BoardGameClient
 import tools.aqua.bgw.net.client.NetworkLogging
 
+/**
+ *  The class [IndigoNetworkClient] describe the Interaction with bgw net as a client
+ *
+ *  @property playerName The player Name is tze name of the player
+ *  @property host The name of the host
+ *  @property secret the secret for the porjekt to make a secure connection
+ *  @property networkService the netWorkservice of the game
+ *  to use the functions of the class
+ */
 class IndigoNetworkClient(
     playerName: String,
     host: String,
@@ -10,5 +19,8 @@ class IndigoNetworkClient(
     var networkService: NetworkService,
     )
     : BoardGameClient(playerName, host, secret, NetworkLogging.VERBOSE) {
+    /**
+     * @property sessionID The SessionID is to connect to the specific game
+     */
     var sessionID: String? = null
 }
