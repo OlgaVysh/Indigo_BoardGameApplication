f1cea2784783d20cebacff91d5a8e8a8f65f9f2b
Joris Schiphorst <joris.schiphorst@tu-dortmund.de>
2023-12-14 14:01:46 +0100

    add IOService stub

---
+++ src/main/kotlin/service/IOService.kt
@@ -0,0 +1,27 @@
+package service
+
+import entity.*
+
+/**
+ * class to read/write files for saving and loading games
+ *
+ * @constructor creates an Instance of the Service tied to a given [RootService]
+ *
+ * @param root [RootService] the IOService belongs to
+ */
+class IOService(private val root: RootService) {
+
+    /**
+     * function for saving a game of Indigo to a file
+     */
+    fun saveGameToFile(){
+
+    }
+
+    /**
+     * function for reading a saved game from a file
+     */
+    fun readGameFromFile(){
+
+    }
+}
