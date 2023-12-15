962da5515213177d323b8172b932a6816db80b3f
Seminay <seminay.cayir@tu-dortmund.de>
2023-12-15 12:48:02 +0100

    implemented the Irish Grover font for the button class.

--- src/main/kotlin/view/Button.kt
+++ src/main/kotlin/view/Button.kt
@@ -11,6 +11,6 @@ class Button (posX : Int = 0, posY : Int = 0, text : String = "Button") :
         width = 300,
         height = 85,
         text = text,
-        font = Font(size = 20, color = Color.WHITE, fontStyle = Font.FontStyle.ITALIC),
+        font = Font(size = 20, color = Color.WHITE, family = "IrishGrover.ttf"),
         visual = ImageVisual("button.png").apply {  }
     )
