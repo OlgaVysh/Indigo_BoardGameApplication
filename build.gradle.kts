14403dd4f797e028fc1a25b8c2876ca1c2e65ef7
Joris Schiphorst <joris.schiphorst@tu-dortmund.de>
2023-12-18 16:59:12 +0100

    add Jackson dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation(group = "tools.aqua", name = "bgw-net-common", version = "0.9")
     implementation(group = "tools.aqua", name = "bgw-net-client", version = "0.9")
     implementation(group = "edu.udo.cs.sopra", name = "ntf", version = "1.0")
+    implementation(group = "com.fasterxml.jackson.module", name = "jackson-module-kotlin", version = "2.13.3")
 }
 
 tasks.distZip {
