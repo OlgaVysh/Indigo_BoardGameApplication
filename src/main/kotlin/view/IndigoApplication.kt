df8ed9281c158c87cf606cfd9ae3d12a63be3c0a
Maria Weisheit <maria.weisheit@tu-dortmund.de>
2023-12-18 21:53:26 +0100

    added values: ConfigurePlayerXScene, JoinGameScene, NewPlayerMenuScene in IndigoApplication

--- src/main/kotlin/view/IndigoApplication.kt
+++ src/main/kotlin/view/IndigoApplication.kt
@@ -22,6 +22,9 @@ class IndigoApplication : BoardGameApplication("Indigo Game") //,Refreshable{
     private val networkScene = NetworkMenuScene()
     private val saveGameScene = SaveGameMenuScene()
     private val gameSavedMenuScene = GameSavedMenuScene()
+    private val newPlayerMenuScene = NewPlayerMenuScene(rootService)
+    private val configurePlayerXScene = ConfigurePlayerXScene(rootService)
+    private val joinGameScene = JoinGameScene(rootService)
 
 
     init {
