599dd4b48bb299346c715a6e353dbd556fc5fe1f
OlgaVysh <75023035+OlgaVysh@users.noreply.github.com>
2023-12-18 21:26:28 +0100

    modified SaveGameMenuScene

--- src/main/kotlin/view/SaveGameMenuScene.kt
+++ src/main/kotlin/view/SaveGameMenuScene.kt
@@ -3,17 +3,13 @@ package view
 import tools.aqua.bgw.core.MenuScene
 import tools.aqua.bgw.visual.ImageVisual
 
-class SaveGameMenuScene : MenuScene(1920, 1080) {
+class SaveGameMenuScene : MenuScene(1920, 1080, background = ImageVisual("SaveMenuScene.png")) {
     private val continueButton = Button(266, 642,528,207, "Continue game", 48)
     private val yesButton = Button(1100,642,528,207, "Yes", 48)
     private val label1 = Label(424, 284, 1072, 116, "Do you want to break of", 86)
     private val label2 = Label(424, 400, 1072, 116, "and save the game?", 86)
     init {
-        background = ImageVisual("SaveMenuScene.png")
         opacity = 1.0
-        addComponents(label1)
-        addComponents(label2)
-        addComponents(continueButton)
-        addComponents(yesButton)
+        addComponents(label1, label2, continueButton, yesButton)
     }
 }
