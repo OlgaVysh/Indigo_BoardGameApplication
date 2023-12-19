9536ca7c511b5525cefa4997fb3c060d0ea2c011
abouzerda <amin.bouzerda@udo.edu>
2023-12-19 10:58:55 +0100

    Removed JDK Version 16 compile target from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -96,11 +96,3 @@ tasks.dokkaHtml.configure {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
 }
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "16"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "16"
-}
