dfaac8a9b5fd7654fe728cd20fa1305a797218c1
mohammadkarkanawi <mohammad.karkanwi@tu-dortmund.de>
2023-12-19 02:44:11 +0100

    tileBlocksExit implementiert

--- src/main/kotlin/service/GameService.kt
+++ src/main/kotlin/service/GameService.kt
@@ -50,7 +50,69 @@ class GameService(private val rootService: RootService) {
         val currentGame = rootService.currentGame
         checkNotNull(currentGame)
 
+        val gate1 = listOf(Coordinate(-4, 1), Coordinate(-4, 2), Coordinate(-4, 3))
+        val gate2 = listOf(Coordinate(-3, 4), Coordinate(-2, 4), Coordinate(-1, 4))
+        val gate3 = listOf(Coordinate(1, 3), Coordinate(2, 2), Coordinate(3, 1))
+        val gate4 = listOf(Coordinate(4, -1), Coordinate(4, -2), Coordinate(4, -3))
+        val gate5 = listOf(Coordinate(1, -4), Coordinate(2, -4), Coordinate(3, -4))
+        val gate6 = listOf(Coordinate(-1, -3), Coordinate(-2, -2), Coordinate(-3, -1))
+
+        if (gate1.contains(space)) {
+            val edge1 = tile.edges[0]
+            val edge2 = getAnotherEdge(edge1, tile)
+            if (edge2 == 5) {
+                return false
+            }
+        }
+        if (gate2.contains(space)) {
+            val edge1 = tile.edges[0]
+            val edge2 = getAnotherEdge(edge1, tile)
+            if (edge2 == 1) {
+                return false
+            }
+        }
+        if (gate3.contains(space)) {
+            val edge1 = tile.edges[1]
+            val edge2 = getAnotherEdge(edge1, tile)
+            if (edge2 == 2) {
+                return false
+            }
+        }
+        if (gate4.contains(space)) {
+            val edge1 = tile.edges[2]
+            val edge2 = getAnotherEdge(edge1, tile)
+            if (edge2 == 3) {
+                return false
+            }
+        }
+        if(gate5.contains(space)) {
+            val edge1 = tile.edges[3]
+            val edge2 = getAnotherEdge(edge1, tile)
+            if (edge2 == 4) {
+                return false
+            }
+        }
+        if (gate6.contains(space)) {
+            val edge1 = tile.edges[4]
+            val edge2 = getAnotherEdge(edge1, tile)
+            if (edge2 == 5) {
+                return false
+            }
+        }
         return true
+    }
+
+
+    private fun getAnotherEdge(egde: Edge, tile: Tile): Int {
+        val paths = tile.paths
+        val edges = tile.edges
+        for (i in 0 until paths.size) {
+            if (edges[i].equals(paths)) {
+                val pathsIndex = paths[i].(egde)
+                val anotherEdge = edges[(i + 1) % 2][(pathsIndex)]
+                return edges.indexOf(anotherEdge)
+            }
+        }
     }
 
     fun checkCollision() {}//:Unit
