2229b32fdeb5dc9e343b6d0f725b9262a5a96514
Xue Ming Chen <xueming.chen@tu-dortmund.de>
2023-12-20 23:33:53 +0100

    test Function to create a test routeTile list

---
+++ src/test/kotlin/service/createTestPlaceTile.kt
@@ -0,0 +1,51 @@
+import entity.Edge
+import entity.Tile
+
+/**
+ * the function create a  test list of route tiles
+ *  @return A mutable List of route tiles
+ */
+fun createTestRouteTile() : MutableList<Tile>{
+    val tile0 = listOf(
+        Pair(Edge.ZERO, Edge.TWO),
+        Pair(Edge.ONE, Edge.FOUR),
+        Pair(Edge.THREE, Edge.FIVE)
+    )
+    val tile1 = listOf(
+        Pair(Edge.TWO, Edge.FIVE),
+        Pair(Edge.ONE, Edge.FOUR),
+        Pair(Edge.ZERO, Edge.THREE)
+    )
+    val tile2 = listOf(
+        Pair(Edge.ZERO, Edge.FIVE),
+        Pair(Edge.ONE, Edge.FOUR),
+        Pair(Edge.TWO, Edge.THREE)
+    )
+    val tile3 = listOf(
+        Pair(Edge.ZERO, Edge.FIVE),
+        Pair(Edge.ONE, Edge.THREE),
+        Pair(Edge.TWO, Edge.FOUR)
+    )
+    val tile4 = listOf(
+        Pair(Edge.ZERO, Edge.FIVE),
+        Pair(Edge.ONE, Edge.TWO),
+        Pair(Edge.THREE, Edge.FOUR)
+    )
+    val routeTiles = mutableListOf<Tile>()
+    for (i in 0 until 14){
+        routeTiles.add(Tile(tile0, mutableMapOf()))
+    }
+    for (i in 0 until 6){
+        routeTiles.add(Tile(tile1, mutableMapOf()))
+    }
+    for (i in 0 until 14){
+        routeTiles.add(Tile(tile2, mutableMapOf()))
+    }
+    for (i in 0 until 14){
+        routeTiles.add(Tile(tile3, mutableMapOf()))
+    }
+    for (i in 0 until 6){
+        routeTiles.add(Tile(tile4, mutableMapOf()))
+    }
+    return routeTiles
+}
