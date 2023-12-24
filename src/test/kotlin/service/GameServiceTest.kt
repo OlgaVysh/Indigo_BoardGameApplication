3f5a540f286f7ee0ccfd110c55f7952b39b8131c
cem-doruk.uenal <cem-doruk.uenal@tu-dortmund.de>
2023-12-24 19:17:33 +0300

    added test cases for startGame and checkPlacement

--- src/test/kotlin/service/GameServiceTest.kt
+++ src/test/kotlin/service/GameServiceTest.kt
@@ -1,17 +1,19 @@
 package service
-/*
+
 
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-*/
+import java.util.*
+import createTestRouteTile
+import entity.*
+
 
 /**
  * Test class for testing the methods in GameService
  */
 class GameServiceTest{
-/*
+
     var rootService : RootService = RootService()
     var gameService : GameService = rootService.gameService
 
@@ -22,13 +24,43 @@ class GameServiceTest{
     }
 
     @Test
-    fun startGameTest(){}
+    fun startGameTest(){
+/*        assertNull(rootService.currentGame)
+        val player1 = Player("Alice", Date(0),TokenColor.WHITE,false)
+        val player2 = Player("Bob", Date(0),TokenColor.PURPLE,false)
+        val player3 = Player("Emily", Date(0),TokenColor.BLUE,false)
+        val player4 = Player("Jack", Date(0),TokenColor.RED,false)
+        rootService.gameService.startGame(players = listOf(player1,player2,player3,player4))
+        assertNotNull(rootService.currentGame)
+        assertEquals(rootService.currentGame?.players, listOf(player1,player2,player3,player4))*/
+    }
     @Test
     fun restartGameTest(){}
     @Test
     fun endGameTest(){}
     @Test
-    fun checkPlacementTest(){}
+    fun checkPlacementTest(){
+/*        val player1 = Player("Alice", Date(0),TokenColor.WHITE,false)
+        val player2 = Player("Bob", Date(0),TokenColor.PURPLE,false)
+        rootService.gameService.startGame(players = listOf(player1,player2))
+        val testCoordinateGate = Coordinate(-2,4)
+
+        val tile2test = Tile(
+            listOf(
+                Pair(Edge.ZERO, Edge.FIVE),
+                Pair(Edge.ONE, Edge.FOUR),
+                Pair(Edge.TWO, Edge.THREE)
+            ))
+        val tile1Test = Tile(
+            listOf(
+                Pair(Edge.TWO, Edge.FIVE),
+                Pair(Edge.ONE, Edge.FOUR),
+                Pair(Edge.ZERO, Edge.THREE)
+            ))
+        //val placementCoordinate = rootService.currentGame?.gameBoard?.gameBoardTiles?.get(Coordinate(-2,4))
+        assertFalse(rootService.gameService.checkPlacement(testCoordinateGate,tile2test))
+        assertTrue(rootService.gameService.checkPlacement(testCoordinateGate,tile1test))*/
+    }
     @Test
     fun checkCollisionTest(){}
 
@@ -52,6 +84,6 @@ class GameServiceTest{
 
     @Test
     fun initializeGemsTest(){}
-*/
+
 
 }
