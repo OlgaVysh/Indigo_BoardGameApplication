e4f8f314e2dae86b7f251ed7993ce805ecd74250
Xue Ming Chen <xueming.chen@tu-dortmund.de>
2023-12-28 21:28:41 +0100

    change Kotlin test to jUnit test

--- src/test/kotlin/service/PlayerTurnServiceTest.kt
+++ src/test/kotlin/service/PlayerTurnServiceTest.kt
@@ -3,9 +3,9 @@ package service
 import org.junit.jupiter.api.Test
 import entity.*
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
 import kotlin.test.BeforeTest
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
 
 class PlayerTurnServiceTest {
 
