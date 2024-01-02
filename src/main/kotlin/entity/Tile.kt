970328cc1a270610d07b0e276db1e7edc77e1729
Xue Ming Chen <xueming.chen@tu-dortmund.de>
2024-01-02 01:35:36 +0100

    all var to val

--- src/main/kotlin/entity/Tile.kt
+++ src/main/kotlin/entity/Tile.kt
@@ -8,6 +8,6 @@ package entity
  * defaults to an empty map.
  * @property edges List of edges on the tile.
  */
-data class Tile(var paths: List<Pair<Edge,Edge>>,var gemEndPosition: MutableMap<Int, Gem> = mutableMapOf()) {
+data class Tile(val paths: List<Pair<Edge,Edge>>,val gemEndPosition: MutableMap<Int, Gem> = mutableMapOf()) {
     val edges: MutableList<Edge> = mutableListOf(Edge.ZERO, Edge.ONE, Edge.TWO, Edge.THREE, Edge.FOUR, Edge.FIVE)
 }
