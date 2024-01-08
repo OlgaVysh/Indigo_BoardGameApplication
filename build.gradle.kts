f03b8ae38e03bd1ee402b09fe7d4a442b140f5ab
Xue Ming Chen <xueming.chen@tu-dortmund.de>
2024-01-08 16:17:31 +0100

    update to 1,1 version ntf

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation(group = "tools.aqua", name = "bgw-gui", version = "0.9")
     implementation(group = "tools.aqua", name = "bgw-net-common", version = "0.9")
     implementation(group = "tools.aqua", name = "bgw-net-client", version = "0.9")
-    implementation(group = "edu.udo.cs.sopra", name = "ntf", version = "1.0")
+    implementation(group = "edu.udo.cs.sopra", name = "ntf", version = "1.1")
     implementation(group = "com.fasterxml.jackson.module", name = "jackson-module-kotlin", version = "2.13.3")
 }
 
