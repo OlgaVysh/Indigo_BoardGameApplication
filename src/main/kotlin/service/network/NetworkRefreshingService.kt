e2847ca782790d011d7871a57675f814ba8f385f
Xue Ming Chen <xueming.chen@tu-dortmund.de>
2024-01-10 21:46:51 +0100

    updated docs

--- src/main/kotlin/service/network/NetworkRefreshingService.kt
+++ src/main/kotlin/service/network/NetworkRefreshingService.kt
@@ -7,10 +7,17 @@ import service.AbstractRefreshingService
  *  for the view Layer
  */
 class NetworkRefreshingService : AbstractRefreshingService()  {
+
+    /**
+     * The function trigger the refresh function for refreshAfterPlayerJoined
+     */
     fun refreshAfterPlayerJoined(playerJoinedName : String){
         onAllRefreshables {refreshAfterPlayerJoined(playerJoinedName) }
     }
 
+    /**
+     * The function trigger the refresh function for refreshAfterPlayerJoined
+     */
     fun refreshAfterPlayerLeaved(playerLeavedName : String){
         onAllRefreshables {refreshAfterPlayerLeaved(playerLeavedName) }
     }
