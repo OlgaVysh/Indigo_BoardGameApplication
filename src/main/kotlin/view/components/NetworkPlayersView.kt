7b58db13f44504386682cac5fe9d57606efad4b8
Xue Ming Chen <xueming.chen@tu-dortmund.de>
2024-01-11 23:52:22 +0100

    private button and label to public

--- src/main/kotlin/view/components/NetworkPlayersView.kt
+++ src/main/kotlin/view/components/NetworkPlayersView.kt
@@ -7,8 +7,8 @@ import tools.aqua.bgw.visual.ColorVisual
 class NetworkPlayersView (posX : Int = 0, posY : Int = 0) :
     Pane<ComponentView>(posX,posY,1643,141, visual = ColorVisual(253,240,216))
 {
-    private val button = Button(1212,16,367,109,"Configure",30)
-    private val label = Label(141,44,956,58,"Player1 : Name", 48)
+   val button = Button(1212,16,367,109,"Configure",30)
+   val label = Label(141,44,956,58,"Player1 : Name", 48)
 
     init {
         this.add(button)
