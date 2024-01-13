bcc3d9d9385811feaba3b82323e60055581b5d2c
Xue Ming Chen <xueming.chen@tu-dortmund.de>
2024-01-13 18:05:42 +0100

    updated JoinButton with the new joinGame Parameter

--- src/main/kotlin/view/JoinGameScene.kt
+++ src/main/kotlin/view/JoinGameScene.kt
@@ -16,7 +16,7 @@ import view.components.Label
  *
  * The layout and design of these components are defined in this class.
  */
-class JoinGameScene(val indigoApp : IndigoApplication) : MenuScene(990, 1080), Refreshable {
+class JoinGameScene(val indigoApp: IndigoApplication) : MenuScene(990, 1080), Refreshable {
     //private val game = rootService.currentGame
 
     private val rootService = indigoApp.rootService
@@ -25,27 +25,31 @@ class JoinGameScene(val indigoApp : IndigoApplication) : MenuScene(990, 1080), R
     private val titleLabel = Label(42, 80, 900, 116, "Configure Player X", 96)
 
     private val nameLabel = Label(80, 370, width = 300, text = "Name : ", fontSize = 48)
-    private val nameInput: TextField = TextField(width = 420, height = 69, posX = 320, posY = 370).
-    apply {  onKeyTyped = {
-        joinButton.isDisabled = this.text.isBlank() || idInput.text.isBlank()
-    }}
+    private val nameInput: TextField = TextField(width = 420, height = 69, posX = 320, posY = 370).apply {
+        onKeyTyped = {
+            joinButton.isDisabled = this.text.isBlank() || idInput.text.isBlank()
+        }
+    }
 
     private val idLabel = Label(80, 535, width = 300, text = "Session ID : ", fontSize = 40)
-    private val idInput: TextField = TextField(width = 420, height = 69, posX = 320, posY = 535).
-    apply {  onKeyTyped = {
-        joinButton.isDisabled = this.text.isBlank() || nameInput.text.isBlank()
-    }}
+    private val idInput: TextField = TextField(width = 420, height = 69, posX = 320, posY = 535).apply {
+        onKeyTyped = {
+            joinButton.isDisabled = this.text.isBlank() || nameInput.text.isBlank()
+        }
+    }
 
-    private val joinButton = Button(247, 800, 528, 207, "Join", 48).
-    apply {isDisabled = nameInput.text.isBlank() || idInput.text.isBlank()
-        var isAi = false
-        if (yesButton.isSelected) isAi = true
-        if (nameInput.text.isNotBlank() && idInput.text.isNotBlank())
-            indigoApp.rootService.networkService.joinGame(
-                name = nameInput.name,
-                sessionID = idLabel.name,
-                isAi = isAi
-            )
+    private val joinButton = Button(247, 800, 528, 207, "Join", 48).apply {
+        isDisabled = nameInput.text.isBlank() || idInput.text.isBlank()
+        onMouseClicked = {
+            var isAi = false
+            if (yesButton.isSelected) isAi = true
+            if (nameInput.text.isNotBlank() && idInput.text.isNotBlank())
+                indigoApp.rootService.networkService.joinGame(
+                    name = nameInput.name,
+                    sessionID = idLabel.name,
+                    isAi = isAi
+                )
+        }
     }
 
     private val aiLabel = Label(80, 700, width = 300, text = "AI : ", fontSize = 48)
@@ -73,7 +77,6 @@ class JoinGameScene(val indigoApp : IndigoApplication) : MenuScene(990, 1080), R
         opacity = 0.5
         // Add components to the scene
         addComponents(titleLabel)
-        addComponents(joinButton)
         addComponents(idLabel)
         addComponents(nameLabel)
         addComponents(aiLabel)
@@ -83,6 +86,7 @@ class JoinGameScene(val indigoApp : IndigoApplication) : MenuScene(990, 1080), R
         addComponents(noButton)
         addComponents(yesLabel)
         addComponents(noLabel)
+        addComponents(joinButton)
         addComponents(textMessageLabel)
 
         // Set alignment for specific labels
@@ -150,6 +154,6 @@ class JoinGameScene(val indigoApp : IndigoApplication) : MenuScene(990, 1080), R
     override fun refreshAfterStartNewJoinedGame() {
         indigoApp.showGameScene(indigoApp.gameScene)
         indigoApp.hideMenuScene()
-       //TODO(//indigoApp.gameScene.refreshAfterStartNewGame)
+        //TODO(//indigoApp.gameScene.refreshAfterStartNewGame)
     }
 }
