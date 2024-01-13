7ec981cb1a0220802eb262b33973a82bb07154ef
Xue Ming Chen <xueming.chen@tu-dortmund.de>
2024-01-13 18:37:50 +0100

    addRefreshables in init

--- src/main/kotlin/view/IndigoApplication.kt
+++ src/main/kotlin/view/IndigoApplication.kt
@@ -16,18 +16,20 @@ class IndigoApplication : BoardGameApplication("Indigo Game"), Refreshable {
     // Central service from which all others are created/accessed
     // also holds the currently active game
 
-    var networkMode : Boolean=false //sagt, ob wir Hotseat oder Network spielen (wird in ModusMenuScene gesetzt)
+    var networkMode: Boolean = false //sagt, ob wir Hotseat oder Network spielen (wird in ModusMenuScene gesetzt)
     val rootService = RootService()
 
     val startScene = NewGameMenuScene(this)
     val modusScene = ModusMenuScene(this)
     val configurePlayersScene = ConfigurePlayersGameScene(this)
+
     //val gatesScene = GateMenuScene(this)
-     val networkScene = NetworkMenuScene(this)
-     val configurePlayerXScene = ConfigurePlayerXScene(this)
-     val joinGameScene = JoinGameScene(this)
-     //val endGameMenuScene = EndGameMenuScene(this)
-     // val aiMenuScene = AIMenuScene(this)
+    val networkScene = NetworkMenuScene(this)
+    val configurePlayerXScene = ConfigurePlayerXScene(this)
+    val joinGameScene = JoinGameScene(this)
+
+    //val endGameMenuScene = EndGameMenuScene(this)
+    // val aiMenuScene = AIMenuScene(this)
     val gameSavedScene = GameSavedMenuScene(this)
     val saveGameScene = SaveGameMenuScene(this)
     val hostGameScene = HostGameScene(this)
@@ -39,6 +41,25 @@ class IndigoApplication : BoardGameApplication("Indigo Game"), Refreshable {
 
     init {
         //das ladet unser IrishGrover Font
+        rootService.addRefreshables(
+            this,
+            startScene,
+            modusScene,
+            configurePlayersScene,
+            //gatesScene,
+            configurePlayerXScene,
+            joinGameScene,
+            networkScene,
+            // endGameMenuScene ,
+            //aiMenuScene,
+            gameSavedScene,
+            saveGameScene,
+            hostGameScene,
+            savedGamesScene,
+            networkConfigureScene,
+            gameScene
+            //newPlayerScene,
+        )
         val resource = this::class.java.getResource("/IrishGrover.ttf")
             ?: throw FileNotFoundException()
         val fontFile = File(resource.toURI())
