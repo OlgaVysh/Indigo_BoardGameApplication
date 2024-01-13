8b8f6558f136dc92a84ab34dbd14a7b6d632199b
OlgaVysh <75023035+OlgaVysh@users.noreply.github.com>
2024-01-13 20:53:52 +0100

    updated IndigoApplication

--- src/main/kotlin/view/IndigoApplication.kt
+++ src/main/kotlin/view/IndigoApplication.kt
@@ -15,6 +15,7 @@ class IndigoApplication : BoardGameApplication("Indigo Game"), Refreshable {
 
     // Central service from which all others are created/accessed
     // also holds the currently active game
+    var aiGame = false //ob es KI Spieler gibt
     var players: MutableList<Player> = mutableListOf() // list of players for the Game
     var networkMode : Boolean=false //sagt, ob wir Hotseat oder Network spielen (wird in ModusMenuScene gesetzt)
     val rootService = RootService()
@@ -29,7 +30,7 @@ class IndigoApplication : BoardGameApplication("Indigo Game"), Refreshable {
     val joinGameScene = JoinGameScene(this)
 
     //val endGameMenuScene = EndGameMenuScene(this)
-    // val aiMenuScene = AIMenuScene(this)
+    val aiMenuScene = AIMenuScene(this)
     val gameSavedScene = GameSavedMenuScene(this)
     val saveGameScene = SaveGameMenuScene(this)
     val hostGameScene = HostGameScene(this)
