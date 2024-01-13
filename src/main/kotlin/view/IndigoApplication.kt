95ae38408888d55394c6aec2c58ad56fee26a803
OlgaVysh <75023035+OlgaVysh@users.noreply.github.com>
2024-01-13 18:53:07 +0100

    added var players to IndigoApplication

--- src/main/kotlin/view/IndigoApplication.kt
+++ src/main/kotlin/view/IndigoApplication.kt
@@ -16,7 +16,7 @@ class IndigoApplication : BoardGameApplication("Indigo Game"), Refreshable {
 
     // Central service from which all others are created/accessed
     // also holds the currently active game
-    var players: MutableList<Player> = mutableListOf()
+    var players: MutableList<Player> = mutableListOf() // list of players for the Game
     var networkMode : Boolean=false //sagt, ob wir Hotseat oder Network spielen (wird in ModusMenuScene gesetzt)
     val rootService = RootService()
 
