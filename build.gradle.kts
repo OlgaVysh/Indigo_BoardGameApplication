513ddb06fd7026dbd62b6d9a40c685bb192b5f50
Xue Ming Chen <xueming.chen@tu-dortmund.de>
2024-01-25 20:06:09 +0100

    dependecies testimplemntation courotines changed to implemtation courotines

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ application {
 
 dependencies {
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation(group = "tools.aqua", name = "bgw-gui", version = "0.9-4-06a99c3-SNAPSHOT")
     implementation(group = "tools.aqua", name = "bgw-net-common", version = "0.9")
     implementation(group = "tools.aqua", name = "bgw-net-client", version = "0.9")
