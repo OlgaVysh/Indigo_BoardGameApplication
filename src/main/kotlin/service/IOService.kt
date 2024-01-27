65dba79808d069b9e2122d2f21c0891b5c5e86f5
Maria Weisheit <maria.weisheit@tu-dortmund.de>
2024-01-27 15:58:10 +0100

    created folder to save the GameState (indigogames) and changed saveGametoFile to a MutuableList<Indigo>

--- src/main/kotlin/service/IOService.kt
+++ src/main/kotlin/service/IOService.kt
@@ -1,10 +1,13 @@
 package service
 
 import com.fasterxml.jackson.databind.module.SimpleModule
-import entity.*
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import entity.*
 import java.io.File
+import kotlin.io.path.Path
+import kotlin.io.path.createDirectories
+
 
 /**
  * class to read/write files for saving and loading games
@@ -38,12 +41,24 @@ class IOService(private val root: RootService) : AbstractRefreshingService() {
      * @param indigo The Indigo game state to be saved.
      * @param path The path to the file where the game state will be saved.
      */
-    fun saveGameToFile(indigo: Indigo, path:String) {
+    /*fun saveGameToFile(indigo: Indigo, path:String) {
         //changed the parameters from indigo: Indigo, path: String to gameStateList: List<Indigo>, path: String
+
         val json = mapper.writeValueAsString(indigo)
         File(path).writeText(json)
 
     }
+
+     */
+    fun saveGameToFile(indigoList: MutableList<Indigo>, path:String) {
+        val dirPath = Path("./indigogames/")
+        dirPath.createDirectories()
+        val json = mapper.writeValueAsString(indigoList)
+        val fileToWrite = File("$dirPath/$path")
+        fileToWrite.createNewFile()
+        fileToWrite.writeText(json)
+    }
+
 
     /**
      * Reads a game state from a file in JSON format.
@@ -51,8 +66,22 @@ class IOService(private val root: RootService) : AbstractRefreshingService() {
      * @param path The path to the file from which the game state will be read.
      * @return The Indigo game state read from the specified file.
      */
+    //List<MyClass> list = mapper.readerForListOf(MyClass.class).readValue(json)
+    fun readGameFromFile(path: String): List<Indigo> {
+        val jsonString = File(path).readText()
+        var indigoList = mutableListOf<Indigo>()
+        indigoList.add(mapper.readValue<Indigo>(jsonString))
+        return indigoList
+    }
+
+    /*
     fun readGameFromFile(path: String): Indigo {
         val jsonString = File(path).readText()
+
         return mapper.readValue<Indigo>(jsonString)
     }
+
+     */
+
+
 }
