346cd04e0c538c055f1a57b5ab9d3d8c8fa15acf
Xue Ming Chen <xueming.chen@tu-dortmund.de>
2024-01-27 12:49:12 +0100

    removed unused Import

--- src/main/kotlin/entity/Indigo.kt
+++ src/main/kotlin/entity/Indigo.kt
@@ -2,7 +2,6 @@ package entity
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo
 import com.fasterxml.jackson.annotation.JsonIdentityReference
-import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.ObjectIdGenerators
 
 /**
